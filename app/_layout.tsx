3e65753d31f5cef03a7c82d5f0e4abede63d54d1
lilMad <amhassani@yahoo.com>
2026-03-30 21:28:54 +1100

    Remove invalid anchor from layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,20 +2,12 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
-//import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
 export default function RootLayout() {
-  
-
   return (
     <>
       <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} /> 
-        <Stack.Screen name="saved" options={{ headerShown: false }} />   
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="saved" options={{ headerShown: false }} />
         <Stack.Screen name="results" options={{ headerShown: false }} />
       </Stack>
       <StatusBar style="auto" />
