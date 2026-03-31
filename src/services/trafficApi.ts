ae4822293885e27a2deb3ca624c38067038e0d59
lilMad <amhassani@yahoo.com>
2026-04-01 00:50:20 +1100

    Remove Edge sidebar metadata

--- src/services/trafficApi.ts
+++ src/services/trafficApi.ts
@@ -1,10 +1,8 @@
 import type { Incident } from "../types/Incident";
 
-const API_KEY = process.env.EXPO_PUBLIC_NSW_API_KEY;
-
-export async function fetchIncidents() {
+export async function fetchIncidents(): Promise<Incident[]> {
   const res = await fetch(
-    "https://api.transport.nsw.gov.au/v1/traffic/hazards",
+    "https://api.transport.nsw.gov.au/v1/live/hazards/incident/all",
     {
       method: "GET",
       headers: {
